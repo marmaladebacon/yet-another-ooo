a1b767f3313497df2c798a1364cc5d8992c3a586
mko <zaeni.redmage@gmail.com>
2018-12-03 18:11:59 +0000

    getting assignees location

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const octokit = require('@octokit/rest')()
+
+async function GetLocation (login) {
+  // https://api.github.com/repos/marmaladebacon/mb-ai-movement/commits?author=marmaladebacon
+  const result = await octokit.users.getByUsername({username: login})
+  return result.location
+}
+
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
@@ -9,7 +17,21 @@ module.exports = app => {
 
   app.on('issues.assigned', async context => {
     console.log(context.payload.issue.assignees)
-    const issueComment = context.issue({ body: 'Hello, thanks for assigning someone to this issue.' })    
+    const possibleAssignees = context.payload.issue.assignees
+    let store = {}
+    let allAssignees = []
+    possibleAssignees.forEach(async (assignee) => {
+      const loc = await GetLocation(assignee.login)
+      store[assignee.login] = {
+        location: loc
+      }
+      allAssignees.push(assignee.login)
+    })
+    let textAdd = ''
+    allAssignees.forEach(login => {
+      textAdd += `\n${login}: ${store[login].location}`
+    })
+    const issueComment = context.issue({ body: `Hello, thanks for assigning someone to this issue.${textAdd}` })
     return context.github.issues.createComment(issueComment)
   })
 
